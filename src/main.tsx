fddd2275c5b7e38e904a91b04c268a7683800651
nightlifex <steven222giese@gmail.com>
2026-09-01 20:30:06 +0200

    Redesign: src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource-variable/sen";
 import { App } from "./App";
 import "./styles.css";
 
@@ -8,3 +9,4 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>,
 );
+
